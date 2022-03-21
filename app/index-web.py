f181b202dc865477b4b063dd6ead869632b6ed22
ablancoze <34425071+ablancoze@users.noreply.github.com>
2022-03-21 10:24:25 +0100

    first commit

---
+++ app/index-web.py
@@ -0,0 +1,13 @@
+#import de las librerias
+from datetime import datetime
+import pyscopg2
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+#variable app para inidicar a flask las rutas de los comandos back
+app = Flask(__name__)
+
+#    
+@app.route('/')
+def index():
+
+    return render_template("cv-alvaro.html")
